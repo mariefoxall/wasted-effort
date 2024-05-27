401151580de2c1285e8e8043099d56e3db85d014
Marie Foxall <66482428+mariefoxall@users.noreply.github.com>
2024-05-27 09:18:02 -0400

    update gallery images

--- src/Exhibitions/Exhibitions.jsx
+++ src/Exhibitions/Exhibitions.jsx
@@ -1,7 +1,12 @@
-import lamp from "../assets/entretemps-lamp.jpg";
-import key from "../assets/entretemps-key.jpg";
-import beadMaze from "../assets/bead-maze-close.jpg";
-import ceramicTable from "../assets/ceramic-table.jpg";
+import cigarettes from "../assets/cigarettes.jpg";
+import lamp from "../assets/portal-lamp-cropped.jpg";
+import lampAndMaze from "../assets/chainmail-lamp-bead-maze.jpg";
+import overall from "../assets/overall.jpg";
+import chair from "../assets/tall-chair.jpg";
+// import lamp from "../assets/entretemps-lamp.jpg";
+// import key from "../assets/entretemps-key.jpg";
+// import beadMaze from "../assets/bead-maze-close.jpg";
+import ceramicCorner from "../assets/ceram-corner.jpg";
 import "./Exhibitions.css";
 
 function Exhibitions() {
@@ -13,23 +18,33 @@ function Exhibitions() {
 
       <img
         className="gallery-image"
+        src={overall}
+        alt="Wide shot of art gallery with multicolored sculptures all over"
+      />
+      <img
+        className="gallery-image-vert"
         src={lamp}
         alt="Multicolored 6-foot tall paper mache lamp and palm tree"
       />
       <img
-        className="gallery-image"
-        src={key}
-        alt="Silver foil paper mache key and extra-large carved wooden key, chained together and hanging on a wall"
+        className="gallery-image-vert"
+        src={lampAndMaze}
+        alt="Ceramic and lucite chainmail lamp hanging above lucite structure with ceramic and wood beads"
       />
       <img
         className="gallery-image"
-        src={beadMaze}
-        alt="Lucite structure with ceramic and wood beads"
+        src={cigarettes}
+        alt="Silver ceramic lustre ashtray full of beaded cigarettes"
       />
       <img
-        className="gallery-image"
-        src={ceramicTable}
-        alt="Pink ceramic table with rounded edges and a wacky multicolored vessel placed on top"
+        className="gallery-image-vert"
+        src={ceramicCorner}
+        alt="Tall ceramic lamp, pink ceramic table with rounded edges and a wacky multicolored vessel placed on top"
+      />
+      <img
+        className="gallery-image-vert"
+        src={chair}
+        alt="Comically tall lime green & clear lucite chair with speckled ceramic bag and giant ceramic cigarette butt"
       />
     </>
   );
