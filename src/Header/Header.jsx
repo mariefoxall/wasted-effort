6d59fdf0f02410b9290ee0943219f315b47cff8f
Marie Foxall <66482428+mariefoxall@users.noreply.github.com>
2024-08-05 09:37:03 -0400

    work on header

--- src/Header/Header.jsx
+++ src/Header/Header.jsx
@@ -1,13 +1,15 @@
 import Menu from "../Menu/Menu";
 import { Link } from "react-router-dom";
 import "./Header.css";
+import bigW from "../assets/w-logo.jpeg";
 
 function Header() {
   return (
     <header>
       <button className="home-button">
-        <Link to="/">
-          <h2 className="big-w">W</h2>{" "}
+        <Link className="home-link" to="/">
+          {/* <h2 className="big-w">W</h2>{" "} */}
+          <img src={bigW} className="big-w" alt="Wasted Effort logo" />
         </Link>
       </button>
       <Menu />
